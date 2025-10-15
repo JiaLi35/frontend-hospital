e363c033a3ae6f4c18dfee02ae9fc76c13e2573d
Jia Li <chin.j3li@gmail.com>
2025-10-15 14:27:38 +0800

    api url pt2.

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.DEV
-  ? "http://localhost:5120/api"
-  : "https://b15-jiali.mak3r.dev/api";
+  ? "http://localhost:5120/api/"
+  : "https://b15-jiali.mak3r.dev/api/";
